88312264495301bc2dc22af88da236a10a56f688
Shosuke1998 <shosuke123@outlook.jp>
2021-09-23 15:37:27 +0900

    first　commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
